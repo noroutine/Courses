1fb0387fadc623102405978bcc7e227131e3be45
Oleksii Khilkevych <oleksiy@noroutine.me>
2024-07-25 17:00:12 +0200

    Accomodate Cutouts

--- app/src/main/java/me/noroutine/courses/MainActivity.kt
+++ app/src/main/java/me/noroutine/courses/MainActivity.kt
@@ -5,14 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import me.noroutine.courses.data.DataSource
 import me.noroutine.courses.model.CourseTopic
 import me.noroutine.courses.ui.theme.CoursesTheme
 
@@ -32,6 +41,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
+                Scaffold { innerPadding ->
+                    CoursesGrid(
+                        DataSource.CourseTopics,
+                        modifier = Modifier
+                            .windowInsetsPadding(WindowInsets.safeDrawing)
+                            .padding(innerPadding)
+                    )
+                }
             }
         }
     }
@@ -40,7 +57,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CourseTopicCard(courseTopic: CourseTopic, modifier: Modifier = Modifier) {
     Card(
-        modifier = Modifier.height(68.dp)
+        modifier = Modifier
+            .height(68.dp)
     ) {
         Row {
             Image(
@@ -84,3 +102,26 @@ fun CourseTopicCardPreview() {
         CourseTopicCard(CourseTopic(R.string.photography, 321, R.drawable.photography))
     }
 }
+
+@Composable
+fun CoursesGrid(courseTopics: List<CourseTopic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 178.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(8.dp)
+    ) {
+        items(courseTopics) { courseTopic ->
+            CourseTopicCard(courseTopic = courseTopic)
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CoursesGridPreview() {
+    CoursesTheme {
+        CoursesGrid(DataSource.CourseTopics)
+    }
+}
+
--- app/src/main/java/me/noroutine/courses/data/DataSource.kt
+++ app/src/main/java/me/noroutine/courses/data/DataSource.kt
@@ -18,6 +18,21 @@ object DataSource {
         CourseTopic(R.string.music, 212, R.drawable.music),
         CourseTopic(R.string.painting, 172, R.drawable.painting),
         CourseTopic(R.string.photography, 321, R.drawable.photography),
+        CourseTopic(R.string.tech, 118, R.drawable.tech),
+
+        CourseTopic(R.string.architecture, 58, R.drawable.architecture),
+        CourseTopic(R.string.crafts, 121, R.drawable.crafts),
+        CourseTopic(R.string.business, 78, R.drawable.business),
+        CourseTopic(R.string.culinary, 118, R.drawable.culinary),
+        CourseTopic(R.string.design, 423, R.drawable.design),
+        CourseTopic(R.string.fashion, 92, R.drawable.fashion),
+        CourseTopic(R.string.film, 165, R.drawable.film),
+        CourseTopic(R.string.gaming, 164, R.drawable.gaming),
+        CourseTopic(R.string.drawing, 326, R.drawable.drawing),
+        CourseTopic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        CourseTopic(R.string.music, 212, R.drawable.music),
+        CourseTopic(R.string.painting, 172, R.drawable.painting),
+        CourseTopic(R.string.photography, 321, R.drawable.photography),
         CourseTopic(R.string.tech, 118, R.drawable.tech)
     )
 }
